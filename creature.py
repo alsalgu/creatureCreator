a69ea5e441e95c82560c494f03146f5edd6d8da7
Al <xochipillitx@gmail.com>
2018-04-27 07:54:07 -0500

    Added horn type, horn show, mane color, and mane genes.

--- creature.py
+++ creature.py
@@ -6,6 +6,9 @@ import random
 eyeDict = {'Red': 'R', 'Rose': 'r', 'Brown': 'B',
            'Blue': 'b', 'Grey': 'G', 'Green': 'g'}
 furDict = {'Black': 'B', 'Brown': 'b', 'White': 'W', 'Wheat': 'w'}
+maneDict = {'Black': 'B', 'Brown': 'b', 'White': 'W', 'Wheat': 'w'}
+hornToggle = {'Yes': 'Y', 'No': 'y'}
+hornType = {'Curved': 'C', 'Straight': 'c'}
 sex = ['Male', 'Female']
 
 allCreatures = []
@@ -22,48 +25,76 @@ class Creature:
             list(furDict.values())) + random.choice(list(furDict.values()))
         self.eyeGene = random.choice(
             list(eyeDict.values())) + random.choice(list(eyeDict.values()))
+        self.maneGene = random.choice(
+            list(maneDict.values())) + random.choice(list(maneDict.values()))
+        self.hornGene = random.choice(
+            list(hornType.values())) + random.choice(list(hornType.values()))
+        self.hornToggled = random.choice(
+            list(hornToggle.values())) + random.choice(list(hornToggle.values()))
         # Default colors in case I miss a genotype combination in the results
         self.furColor = 'Brown'
         self.eyeColor = 'Green'
+        self.maneColor = 'White'
+        self.hornShow = 'Yes'
+        self.hornShape = 'Straight'
         # Placeholders for parentage
-        self.mom = 'Unknown'
-        self.dad = 'Unknown'
+        self.parent1 = 'Unknown'
+        self.parent2 = 'Unknown'
         # Color RNG based on respective gene
         getFurColor(self, self.furGene)
         getEyeColor(self, self.eyeGene)
+        getManeColor(self, self.maneColor)
+        getHornShow(self, self.hornToggled)
+        getHornType(self, self.hornGene)
         # Add creature to list for easy access later.
         allCreatures.append(self.name)
 
 
 class Baby:
-    def __init__(self, mom, dad):
-        if mom.sex == 'Female' and dad.sex == 'Male':
-            self.sex = random.choice(sex)
-            self.name = input("Congrats, it's a " + self.sex + "! What would you like to name the baby? ")
-            self.mom = mom.name
-            self.dad = dad.name
-            # Generate fur gene but from mom and dad's genes by selecting
-            # one char from each
-            self.furGene = random.choice(
-                mom.furGene) + random.choice(dad.furGene)
-            self.furColor = "Brown"
-            self.eyeGene = random.choice(
-                mom.eyeGene) + random.choice(dad.eyeGene)
-            self.eyeColor = "Green"
-            getEyeColor(self, self.eyeGene)
-            getFurColor(self, self.furGene)
-            babyCreatures.append(self.name)
-        else:
-            print('Parents are incompatible')
-            return
+    def __init__(self, parent1, parent2):
+        self.sex = random.choice(sex)
+        self.name = input("Congrats, it's a " + self.sex +
+                          "! What would you like to name the baby? ")
+        self.parent1 = parent1.name
+        self.parent2 = parent2.name
+        # Generate gene genotype with parents' values
+        self.furGene = random.choice(
+            parent1.furGene) + random.choice(parent2.furGene)
+        self.eyeGene = random.choice(
+            parent1.eyeGene) + random.choice(parent2.eyeGene)
+        self.maneGene = random.choice(
+            parent1.maneGene) + random.choice(parent2.maneGene)
+        self.hornGene = random.choice(
+            parent1.hornGene) + random.choice(parent2.hornGene)
+        self.hornToggled = random.choice(
+            parent1.hornGene) + random.choice(parent2.hornGene)
+        # Default colors in case getFunctions fail
+        self.furColor = "Brown"
+        self.eyeColor = "Green"
+        self.maneColor = "White"
+        self.hornShow = "Yes"
+        self.hornShape = "Curved"
+        # Functions to retrieve genes' phenotypes
+        getEyeColor(self, self.eyeGene)
+        getFurColor(self, self.furGene)
+        getManeColor(self, self.maneGene)
+        getHornShow(self, self.hornToggled)
+        getHornType(self, self.hornGene)
+        # Add to a list of babies
+        babyCreatures.append(self.name)
 
 
-def listPedigree(name):
+def getPedigree(name):
     # Prints creature variables in a nice paragraph
     print(name.name + " has " + name.furColor + " fur.\n" +
           name.name + " has " + name.eyeColor + " eyes.\n" +
-          name.name + " is a " + name.sex + ".\n" +
-          "Parents: " + name.mom + ", " + name.dad)
+          name.name + " has a " + name.maneColor + " mane.\n" +
+          name.name + " is a " + name.sex)
+    if name.hornShow == "Show":
+        print(name.name + " has horns and they are " + name.hornShape + ".")
+    else:
+        print(name.name + " has no horns.")
+    print("Parents: " + name.parent1 + ", " + name.parent2)
 
 
 def getEyeColor(creature, gene):
@@ -126,3 +157,47 @@ def getFurColor(creature, gene):
             outcome = []
             outcome.append('Caramel')
         creature.furColor = random.choice(outcome)
+
+
+def getManeColor(creature, gene):
+    # B - Black, W - White
+    # b - Brown, w - Wheat
+    # Works exactly the same as getEyeColor but with furDict
+    if any(map(str.isupper, gene)):
+        outcome = []
+        if ('B' in gene):
+            outcome.append('Black')
+        if ('W' in gene):
+            outcome.append('White')
+        if ('W' in gene) and ('B' in gene):
+            outcome = []
+            outcome.append('Grey')
+        creature.maneColor = random.choice(outcome)
+    else:
+        outcome = []
+        if ('b' in gene):
+            outcome.append('Brown')
+        if ('w' in gene):
+            outcome.append('Wheat')
+        if ('b' in gene) and ('w' in gene):
+            outcome = []
+            outcome.append('Caramel')
+        creature.maneColor = random.choice(outcome)
+
+
+def getHornShow(creature, gene):
+    # Y - yes, y - n.
+    if any(map(str.isupper, gene)):
+        creature.hornShow = 'Show'
+    else:
+        creature.hornShow = 'NoShow'
+
+
+def getHornType(creature, gene):
+    # hornType = {'Curved': 'C', 'Straight': 'c'}
+    if gene == 'CC' or 'Cc':
+        creature.hornShape = 'Curved'
+    if gene == 'cC':
+        creature.hornShape = 'Spiral'
+    if gene == 'cc':
+        creature.hornShape = 'Straight'
